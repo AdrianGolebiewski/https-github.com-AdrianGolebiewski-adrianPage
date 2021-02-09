d3513050c479bfbc84b2b68470204e424227b31a
Adrian Gołębiewski <adrian.golebiewsk@gmail.com>
2021-02-09 12:13:30 +0100

    zmiany

--- js/main.js
+++ js/main.js
@@ -21,3 +21,59 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+/*const numbers = [1,10,'test', 3,4,5];
+
+
+function multiply(x){
+  console.log(x * 2);
+};
+
+
+numbers.map(multiply);
+
+
+const color = ['red' ,'blue' ,'orange'];
+console.log(color);
+
+
+function powieksz(color){
+  console.log(color.toUpperCase());
+};
+
+
+
+color.map(powieksz)
+
+const nums = [1, 2, 3 ]
+const abcd = ['a', 'b', 'c']
+
+
+
+const newARr = nums.concat(abcd)
+
+console.log(newARr);*/
+
+
+
+/*const numbers = [1,2,3,4]
+
+const drinks = ['pepsi', 'woda', 'piwo'];
+const meals = ['schabowy','ziemniak', 'surówka'];
+
+const menu = [...drinks, ...meals];
+
+console.log(menu);*/
+
+const numbers =[ 1,22,36,40,50,60,75,87,90,100]
+
+for (let number of numbers){
+
+  if(number % 2 === 0) {
+    console.log(`wynik jest parzysty ${number}`);
+  }
+  else{
+   console.log(`wynik jest nieparzysty ${number}`);
+  }
+
+}
